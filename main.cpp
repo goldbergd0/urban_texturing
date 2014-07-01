14c12212549046067c2cb0e2a46fe419b9a0f2d1
Daniel S. Goldberg <DGoldberg92@gmail.com>
2014-07-01 12:55:57 -0400

    Checking memory stuff

--- main.cpp
+++ main.cpp
@@ -17,19 +17,12 @@ int main() {
   
   MyCloud c;
   
-  std::cout<<c.readPly(dir+plyname)<<"\n";
-  std::cout<<c.readPatchInfo(dir)<<"\n";
-  std::cout<<c.readCameras(dir)<<"\n";
-  
-  std::string line;
-  std::string fl("-2.17703 1.54542 -8.80738 1 \n"
-                  "-1.24213 123.123 124.120");
-  std::istringstream ss;
-  std::istringstream ss2;
-  ss.str(fl);
-  float f(0);
-  getline(ss,line);
-  ss2.str(line);
-  ss2>>f;
-  std::cout<<f<<"\n";
+  //std::cout<<c.readPly(dir+plyname)<<"\n";
+  //std::cout<<c.readPatchInfo(dir)<<"\n";
+  //std::cout<<c.readCameras(dir)<<"\n";
+  double a;
+  size_t SIZE(2000000);
+  std::vector<Patch> A;
+  for (int i=0;i<SIZE;i++) A.push_back(Patch p());
+  std::cin.get();
 }
